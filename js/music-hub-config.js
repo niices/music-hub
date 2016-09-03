a0c86f71e828fa953e68fbae205dc519460d217f
niices <niices@outlook.com>
2016-09-03 14:51:33 +0800

    next play

--- js/music-hub-config.js
+++ js/music-hub-config.js
@@ -1,59 +1,82 @@
 window.onload = function() {
-    // 网易云歌单ID
-    // 登陆网页版网易云音乐，进入歌单详情后，在url中可找到歌单id，例：'http://music.163.com/#/playlist?id=112212728')
-    IcePlayer.netEasePlayListId = 112212728;
-
-    // 请求并解析网易云音乐歌单信息
-    IcePlayer.netEase = function() {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', "http://music.163.com/api/playlist/detail?id=" + IcePlayer.netEasePlayListId);
-        xhr.send();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                document.getElementById("content").innerHTML = xhr.responseText;
-            }
-        };
-    }
-
-    IcePlayer.netEase()
+    /**
+     * 配置文件
+     */
+    // 歌曲来源：来自网易云音乐歌单信息请填 true ，来自本地自定义歌曲信息请填 false 。
+    IcePlayer.musicSource = true;
+    // 自动播放
+    IcePlayer.autoPlay = true;
+    IcePlayer.defaultShow = true;
+    // 网易云歌单ID：登陆网页版网易云音乐，进入歌单详情后，在url中可找到歌单id，例：'http://music.163.com/#/playlist?id=445719932')。
+    IcePlayer.netEasePlayListId = 445719932;
 
 
-
-
-
-
-
-    // 手动配置歌曲信息
+    /**
+     * 自定义歌曲信息
+     */
     IcePlayer.localList = [{
         src: 'http://niices.qiniudn.com/%E5%B0%8F%E5%B9%B8%E8%BF%90.mp3',
+        cover: 'http://my-typecho-blog.b0.upaiyun.com/usr/uploads/2016/02/462344463.jpg',
         title: '小幸运',
         author: '田馥甄',
-        cover: 'http://my-typecho-blog.b0.upaiyun.com/usr/uploads/2016/02/462344463.jpg',
+        lrc: '[00:24.600]温柔的晚风\n[00:27.830]轻轻吹过 爱人的梦中\n[00:36.690]温柔的晚风\n[00:39.129]轻轻吹过 故乡的天空\n[00:47.690]温柔的晚风\n[00:50.749]轻轻吹过 城市的灯火\n[00:59.119]今夜的晚风\n[01:02.439]你去哪里 请告诉我\n[01:08.249]\n[01:10.879]温柔的晚风\n[01:14.590]轻轻吹过 爱人的梦中\n[01:22.179]温柔的晚风\n[01:25.549]轻轻吹过 故乡的天空\n[01:33.809]温柔的晚风\n[01:37.539]轻轻地吹过 城市的灯火\n[01:46.509]今夜的晚风\n[01:49.919]你要去哪里 请告诉我\n[01:56.419]\n[02:37.140]温柔的晚风\n[02:40.740]轻轻吹过 爱人的梦中\n[02:49.060]温柔的晚风\n[02:52.370]轻轻吹过 故乡的天空\n[03:00.680]温柔的晚风\n[03:03.860]轻轻吹过 城市的灯火\n[03:12.190]今夜的晚风\n[03:15.440]你要去哪里 请告诉我\n[03:21.370]\n[03:23.620]温柔的晚风\n[03:27.090]轻轻吹过 爱人的梦中\n[03:35.280]温柔的晚风\n[03:39.570]轻轻吹过 故乡的天空\n[03:47.620]温柔的晚风\n[03:50.880]轻轻地吹过 城市的灯火\n[03:59.180]今夜的晚风\n[04:02.680]你要去哪里 请告诉我\n[04:08.800]\n[04:33.830]温柔的晚风\n[04:37.350]请你带走 我昨天的梦\n[04:45.350]今夜的晚风\n[04:48.960]我要去哪里 请告诉我\n[04:59.690]\n'
     }, {
         src: 'http://niices.qiniudn.com/%E5%B0%8F%E5%B9%B8%E8%BF%90.mp3',
+        cover: 'http://my-typecho-blog.b0.upaiyun.com/usr/uploads/2016/02/462344463.jpg',
         title: '小幸运',
         author: '田馥甄',
-        cover: 'http://my-typecho-blog.b0.upaiyun.com/usr/uploads/2016/02/462344463.jpg',
+        lrc: '[00:24.600]温柔的晚风\n[00:27.830]轻轻吹过 爱人的梦中\n[00:36.690]温柔的晚风\n[00:39.129]轻轻吹过 故乡的天空\n[00:47.690]温柔的晚风\n[00:50.749]轻轻吹过 城市的灯火\n[00:59.119]今夜的晚风\n[01:02.439]你去哪里 请告诉我\n[01:08.249]\n[01:10.879]温柔的晚风\n[01:14.590]轻轻吹过 爱人的梦中\n[01:22.179]温柔的晚风\n[01:25.549]轻轻吹过 故乡的天空\n[01:33.809]温柔的晚风\n[01:37.539]轻轻地吹过 城市的灯火\n[01:46.509]今夜的晚风\n[01:49.919]你要去哪里 请告诉我\n[01:56.419]\n[02:37.140]温柔的晚风\n[02:40.740]轻轻吹过 爱人的梦中\n[02:49.060]温柔的晚风\n[02:52.370]轻轻吹过 故乡的天空\n[03:00.680]温柔的晚风\n[03:03.860]轻轻吹过 城市的灯火\n[03:12.190]今夜的晚风\n[03:15.440]你要去哪里 请告诉我\n[03:21.370]\n[03:23.620]温柔的晚风\n[03:27.090]轻轻吹过 爱人的梦中\n[03:35.280]温柔的晚风\n[03:39.570]轻轻吹过 故乡的天空\n[03:47.620]温柔的晚风\n[03:50.880]轻轻地吹过 城市的灯火\n[03:59.180]今夜的晚风\n[04:02.680]你要去哪里 请告诉我\n[04:08.800]\n[04:33.830]温柔的晚风\n[04:37.350]请你带走 我昨天的梦\n[04:45.350]今夜的晚风\n[04:48.960]我要去哪里 请告诉我\n[04:59.690]\n'
     }, ];
-    // IcePlayer.config = {
-    //     playlistId: 112212728,
-    //     autoPlay: true,
-    //     defaultShow: true,
-    // };
-    IcePlayer.config = {
-        playList: IcePlayer.localList,
-        autoPlay: true,
-        defaultShow: true,
+
+
+    /**
+     * 初始化播放器
+     */
+    if (!IcePlayer.musicSource) {
+        IcePlayer.init({
+            playList: IcePlayer.localList,
+            autoPlay: IcePlayer.autoPlay,
+            defaultShow: IcePlayer.defaultShow,
+        })
     }
 
-
-
-    // 配置文件
-    //
-    IcePlayer.config = {
-        playList: IcePlayer.localList,
-        autoPlay: true,
-        defaultShow: true,
+    /**
+     * 请求并解析网易云音乐歌单信息
+     */
+    if (IcePlayer.musicSource) {
+        IcePlayer.netEaseList = [];
+        IcePlayer.netEaseHandle = function() {
+            var data;
+            IcePlayer.utils.ajax({
+                url: "http://www.niices.com/api/netEaseMusic/?playlist_id=" + IcePlayer.netEasePlayListId,
+                success: function(responseText) {
+                    data = JSON.parse(responseText);
+                    for (var i = 0; i < data.result.trackCount; i++) {
+                        var musicInfo = {};
+                        var datum = data.result.tracks[i];
+                        var datumArtistLen = datum.artists.length;
+                        // 基本信息
+                        musicInfo.id = datum.id;
+                        musicInfo.src = datum.mp3Url;
+                        musicInfo.cover = datum.album.blurPicUrl;
+                        musicInfo.title = datum.name;
+                        musicInfo.author = datum.artists[0].name;
+                        for (var j = 1; j < datumArtistLen; j++) {
+                            musicInfo.author += ("/" + datum.artists[j].name);
+                        };
+                        IcePlayer.netEaseList.push(musicInfo);
+                    }
+                    IcePlayer.init({
+                        playList: IcePlayer.netEaseList,
+                        autoPlay: IcePlayer.autoPlay,
+                        defaultShow: IcePlayer.defaultShow,
+                    })
+                },
+                fail: function() {
+                    console.error("拉取歌单失败")
+                }
+            })
+        }
+        IcePlayer.netEaseHandle()
     }
-    IcePlayer.init(IcePlayer.config);
 }
--- js/music-hub.js
+++ js/music-hub.js
@@ -49,6 +49,23 @@ IcePlayer.utils = {
         var curSec = tempSec < 10 ? '0' + tempSec : tempSec;
         return curMin + ':' + curSec;
     },
+
+    ajax: function(options) {
+        var options = options || {};
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', options.url);
+        xhr.send(null);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                var status = xhr.status;
+                if (status >= 200 && status < 300) {
+                    options.success && options.success(xhr.responseText, xhr.responseXML);
+                } else {
+                    options.fail && options.fail(status);
+                }
+            }
+        };
+    }
 }
 
 /**
@@ -56,82 +73,103 @@ IcePlayer.utils = {
  */
 
 IcePlayer.init = function(options) {
-    // 检查必须项
-    // if (!playlistId) {
-    //     console.error('必须指定歌单哦~');
-    //     return;
-    // }
-
+    // 初始状态数据
     var defaultState = {};
-    defaultState.ctrlModImgSrc = "images/switch.png";
-    defaultState.volToggleBtnImgSrc = "images/vol-on.png";
-    defaultState.volToggleBtnImgAlt = "vol-on";
+    defaultState.index = 0;
+    defaultState.playMod = 0;
     // 判断音乐是否自动播放
     if (options.autoPlay) {
         defaultState.autoPlay = "autoplay";
         defaultState.playerShowImgSrc = "images/pause.png";
         defaultState.ctrlPlayImgSrc = "images/pause.png";
-        defaultState.ctrlPlayImgAlt = "pause";
+        defaultState.ctrlPlayImgTitle = "暂停";
     } else {
         defaultState.autoPlay = "";
         defaultState.playerShowImgSrc = "images/play.png";
         defaultState.ctrlPlayImgSrc = "images/play.png";
-        defaultState.ctrlPlayImgAlt = "play";
+        defaultState.ctrlPlayImgTitle = "播放";
     }
 
     // 初始化播放器
     var target = document.querySelector('#music_hub_content');
     HTMLcontent = '<div id="music_hub">';
-    HTMLcontent += '    <audio src="' + options.src + '" preload="auto"' + defaultState.autoPlay + '></audio>';
+    HTMLcontent += '    <audio src="' + options.playList[0].src + '" preload="auto"' + defaultState.autoPlay + '></audio>';
     HTMLcontent += '        <div class="iceplayer">';
     HTMLcontent += '        <div class="info">';
-    HTMLcontent += '            <div class="cover"><img src="' + options.cover + '" alt="cd-cover"></div>';
+    HTMLcontent += '            <div class="cover"><img src="' + options.playList[0].cover + '" title="cd-cover"></div>';
     HTMLcontent += '            <div class="meta">';
-    HTMLcontent += '                <div class="meta_title">' + options.title + '</div>';
-    HTMLcontent += '                <div class="meta_author">' + options.author + '</div>';
+    HTMLcontent += '                <div class="meta_title">' + options.playList[0].title + '</div>';
+    HTMLcontent += '                <div class="meta_author">' + options.playList[0].author + '</div>';
     HTMLcontent += '            </div>';
     HTMLcontent += '        </div>';
-    HTMLcontent += '        <div class="ctrl_play"><img src="' + defaultState.ctrlPlayImgSrc + '" alt="' + defaultState.ctrlPlayImgAlt + '" class="ctrl_play_img"></div>';
+    HTMLcontent += '        <div class="ctrl_play"><img src="' + defaultState.ctrlPlayImgSrc + '" title="' + defaultState.ctrlPlayImgTitle + '" class="ctrl_play_img"></div>';
     HTMLcontent += '        <div class="ctrl">';
-    HTMLcontent += '            <div class="ctrl_list"><img src="images/list.png" alt="list"></div>';
-    HTMLcontent += '            <div class="ctrl_lrc"><img src="images/lrc.png" alt="lrc"></div>';
-    HTMLcontent += '            <div class="ctrl_mod"><img src="' + defaultState.ctrlModImgSrc + '" alt="mod"></div>';
+    HTMLcontent += '            <div class="ctrl_list" title="歌单"><img src="images/list.png"></div>';
+    HTMLcontent += '            <div class="ctrl_lrc" title="歌词"><img src="images/lrc.png"></div>';
+    HTMLcontent += '            <div class="ctrl_mod" title="顺序播放"><img src="images/order.png"></div>';
     HTMLcontent += '            <div class="ctrl_vol">';
-    HTMLcontent += '                <img src="' + defaultState.volToggleBtnImgSrc + '" alt="' + defaultState.volToggleBtnImgAlt + '" id="vol_toggle_btn">';
-    HTMLcontent += '                <div class="vol_progress_inner"><div class="vol_progress_bar"><img src="images/dot.png" alt="dot" id="vol_progress_dot"></div></div>';
+    HTMLcontent += '                <img src="images/vol-on.png" title="静音" id="vol_toggle_btn">';
+    HTMLcontent += '                <div class="vol_progress_inner"><div class="vol_progress_bar"><img src="images/dot.png" id="vol_progress_dot"></div></div>';
     HTMLcontent += '            </div>';
-    HTMLcontent += '            <div class="ctrl_next"><img src="images/next.png" alt="next"></div>';
+    HTMLcontent += '            <div class="ctrl_next" title="下一首"><img src="images/next.png"></div>';
     HTMLcontent += '        </div>';
-    HTMLcontent += '        <div class="remaining_sign"><img src="images/clock.png" alt="mod"><span class="remaining_sign_text"></span></div>';
-    HTMLcontent += '        <div class="loading_sign"><img src="images/loading.png" alt="loading"><span class="loading_sign_text">loading</span></div>';
+    HTMLcontent += '        <div class="remaining_sign"><img src="images/clock.png" title="mod"><span class="remaining_sign_text"></span></div>';
+    HTMLcontent += '        <div class="loading_sign"><img src="images/loading.png" title="loading"><span class="loading_sign_text">loading</span></div>';
     HTMLcontent += '        <div class="timeline_bg"><div class="timeline"><div class="timeline_passed"></div></div></div>';
-    HTMLcontent += '        <div class="hide" id="playerHide"><img src="images/close.png" alt="hide"></div>';
+    HTMLcontent += '        <div class="hide" id="playerHide"><img src="images/close.png" title="关闭"></div>';
     HTMLcontent += '        </div>';
-    HTMLcontent += '    <div class="lrc"></div>';
-    HTMLcontent += '    <div class="list"></div>';
+    HTMLcontent += '    <div class="lrc" style="height: 0px;">';
+    HTMLcontent += '    </div>';
+    HTMLcontent += '    <div class="list" style="height: 0px;">';
+    HTMLcontent += '        <ul class="list_box">';
+    for (var item in options.playList) {
+        HTMLcontent += '            <li class="list_one">';
+        HTMLcontent += '                <div class="cur"></div>';
+        HTMLcontent += '                <div class="index">' + (parseInt(item)+1) + '</div>';
+        HTMLcontent += '                <div class="name" title="' + options.playList[item].title + '">' + options.playList[item].title + '</div>';
+        HTMLcontent += '                <div class="by">' + options.playList[item].author + '</div>';
+        HTMLcontent += '            </li>';
+    }
+    HTMLcontent += '        </ul>';
+    HTMLcontent += '    </div>';
     HTMLcontent += '</div>';
-    HTMLcontent += '<div class="show" id="playerShow"><img src="' + defaultState.playerShowImgSrc + '" alt="show"></div>';
+    HTMLcontent += '<div class="show" id="playerShow"><img src="' + defaultState.playerShowImgSrc + '" title="播放器"></div>';
 
     target.innerHTML = HTMLcontent;
 
     // 获取所需节点
-    var musicHub = document.querySelector('#music_hub');
-    var btnShow = document.querySelector('#playerShow');
+    var music_hub_content = document.querySelector('#music_hub_content');
+    var musicHub = music_hub_content.querySelector('#music_hub');
+    var btnShow = music_hub_content.querySelector('#playerShow');
     var showPlayerImg = btnShow.querySelector('img');
-    var btnHide = document.querySelector('#playerHide');
-    var ctrlPlay = document.querySelector('.ctrl_play');
-    var ctrlPlayImg = document.querySelector('.ctrl_play_img');
+    var btnHide = music_hub_content.querySelector('#playerHide');
+    var ctrlPlay = music_hub_content.querySelector('.ctrl_play');
+    var ctrlPlayImg = music_hub_content.querySelector('.ctrl_play_img');
     var audio = musicHub.querySelector('audio');
-    var volToggleBtnImg = document.querySelector('#vol_toggle_btn');
-    var ctrlVol = document.querySelector('.ctrl_vol');
-    var volProgressInner = document.querySelector('.vol_progress_inner');
-    var volProgressBar = document.querySelector('.vol_progress_bar');
-    var volProgressDot = document.querySelector('#vol_progress_dot');
+    var info = musicHub.querySelector('.info');
+    var ctrlVol = music_hub_content.querySelector('.ctrl_vol');
+    var volToggleBtnImg = music_hub_content.querySelector('#vol_toggle_btn');
+    var volProgressInner = music_hub_content.querySelector('.vol_progress_inner');
+    var volProgressBar = music_hub_content.querySelector('.vol_progress_bar');
+    var volProgressDot = music_hub_content.querySelector('#vol_progress_dot');
     var remainingSign = musicHub.querySelector('.remaining_sign');
     var loadingSign = musicHub.querySelector('.loading_sign');
     var remainingSignText = remainingSign.querySelector('.remaining_sign_text');
-    var timelineBg = document.querySelector('.timeline_bg');
-    var timelinePassed = document.querySelector('.timeline_passed');
+    var timelineBg = music_hub_content.querySelector('.timeline_bg');
+    var timelinePassed = music_hub_content.querySelector('.timeline_passed');
+    var ctrlList = music_hub_content.querySelector('.ctrl_list');
+    var list = music_hub_content.querySelector('.list');
+    var ctrlLrc = music_hub_content.querySelector('.ctrl_lrc');
+    var lrc = music_hub_content.querySelector('.lrc');
+    var ctrlNext = music_hub_content.querySelector('.ctrl_next');
+    var musicCoverImg = info.querySelector('.cover>img');
+    var musicTitle = info.querySelector('.meta_title');
+    var musicAuthor = info.querySelector('.meta_author');
+    var ctrlMod = musicHub.querySelector('.ctrl_mod');
+    var ctrlModImg = ctrlMod.querySelector('img');
+    var list = musicHub.querySelector('.list');
+    var listOne = list.querySelectorAll('.list_one');
+
 
     // 设置初始音量为60%
     audio.volume = 0.6;
@@ -141,6 +179,9 @@ IcePlayer.init = function(options) {
     if (options.defaultShow) {
         setTimeout(showPlayer, 100);
     }
+
+    // 默认播放歌单第一首,此为背景
+    IcePlayer.utils.addClass(listOne[0], "current_playing");
 
     eventInit();
 
@@ -152,6 +193,10 @@ IcePlayer.init = function(options) {
         volToggleBtnImg.addEventListener('click', volToggle);
         ctrlVol.addEventListener('click', volProgress);
         timelineBg.addEventListener('click', timeProgress);
+        ctrlList.addEventListener('click', listToggle);
+        ctrlLrc.addEventListener('click', lrcToggle);
+        ctrlMod.addEventListener('click', ctrlPlayMod);
+        ctrlNext.addEventListener('click', nextPlay);
 
         // 播放器事件
         audio.addEventListener('canplaythrough', handleCanPlayThrough);
@@ -174,8 +219,6 @@ IcePlayer.init = function(options) {
         var curTime = audio.currentTime;
         var playPercent = 100 * (curTime / duration);
         var remTime = duration - curTime;
-
-        console.log(remTime);
 
         timelinePassed.style.width = playPercent.toFixed(2) + '%';
         remainingSignText.innerText = IcePlayer.utils.parseSec(remTime);
@@ -203,31 +246,33 @@ IcePlayer.init = function(options) {
 
     // 播放/暂停
     function playToggle() {
-        if (ctrlPlayImg.alt === "play") {
+        if (ctrlPlayImg.title === "播放") {
             audio.play();
             showPlayerImg.src = "../images/pause.png";
             ctrlPlayImg.src = "../images/pause.png";
-            ctrlPlayImg.alt = "pause";
+            ctrlPlayImg.title = "暂停";
         } else {
             audio.pause();
             showPlayerImg.src = "../images/play.png";
             ctrlPlayImg.src = "../images/play.png";
-            ctrlPlayImg.alt = "play";
+            ctrlPlayImg.title = "播放";
         }
     }
 
     // 音量开/关
     function volToggle() {
-        if (volToggleBtnImg.alt === "vol-off") {
-            audio.muted = false;
-            volToggleBtnImg.src = "../images/vol-on.png";
-            volToggleBtnImg.alt = "vol-on";
-            IcePlayer.utils.removeClass(volProgressDot, "ctrl_vol_off");
-        } else {
+        if (volToggleBtnImg.title === "静音") {
             audio.muted = true;
             volToggleBtnImg.src = "../images/vol-off.png";
-            volToggleBtnImg.alt = "vol-off";
+            volToggleBtnImg.title = "取消静音";
             IcePlayer.utils.addClass(volProgressDot, "ctrl_vol_off");
+            IcePlayer.utils.addClass(volProgressBar, "vol_progress_bar_off");
+        } else {
+            audio.muted = false;
+            volToggleBtnImg.src = "../images/vol-on.png";
+            volToggleBtnImg.title = "静音";
+            IcePlayer.utils.removeClass(volProgressDot, "ctrl_vol_off");
+            IcePlayer.utils.removeClass(volProgressBar, "vol_progress_bar_off");
         }
     }
 
@@ -241,5 +286,100 @@ IcePlayer.init = function(options) {
         }
     }
 
-    console.log(options.playList);
+    // 歌单显示/隐藏
+    function listToggle() {
+        if (list.style.height == "0px" && lrc.style.height == "0px") {
+            list.style.height = "210px";
+            musicHub.style.height = "302px";
+            ctrlList.style.opacity = "1";
+        } else if (list.style.height == "0px" && lrc.style.height == "80px") {
+            lrc.style.height = "0px";
+            ctrlLrc.style.opacity = "0.7";
+            list.style.height = "210px";
+            musicHub.style.height = "302px";
+            ctrlList.style.opacity = "1";
+        } else {
+            list.style.height = "0px";
+            musicHub.style.height = "92px";
+            ctrlList.style.opacity = "0.7";
+        }
+    }
+
+    // 歌词显示/隐藏
+    function lrcToggle() {
+        if (lrc.style.height == "0px" && list.style.height == "0px") {
+            lrc.style.height = "80px";
+            musicHub.style.height = "172px";
+            ctrlLrc.style.opacity = "1";
+        } else if (lrc.style.height == "0px" && list.style.height == "210px") {
+            list.style.height = "0px";
+            ctrlList.style.opacity = "0.7";
+            lrc.style.height = "80px";
+            musicHub.style.height = "172px";
+            ctrlLrc.style.opacity = "1";
+        } else {
+            lrc.style.height = "0px";
+            musicHub.style.height = "92px";
+            ctrlList.style.opacity = "0.7";
+        }
+    }
+
+    // 下一首
+    function nextPlay() {
+        if (defaultState.playMod == 0) {
+            orderPlay();
+        } else if (defaultState.playMod == 1) {
+            repeatPlay();
+        } else {
+            shufflePlay();
+        }
+    }
+
+    // 切歌
+    function switchMusic(index) {
+        audio.src = options.playList[index].src;
+        musicCoverImg.src = options.playList[index].cover;
+        musicTitle.innerHTML = options.playList[index].title;
+        musicAuthor.innerHTML = options.playList[index].author;
+        IcePlayer.utils.addClass(listOne[index], "current_playing");
+    }
+
+    // 顺序播放
+    function orderPlay() {
+        IcePlayer.utils.removeClass(listOne[defaultState.index], "current_playing");
+        ++defaultState.index;
+        if (defaultState.index >= options.playList.length) {
+            defaultState.index = 0;
+        }
+        switchMusic(defaultState.index);
+    }
+
+    // 单曲循环
+    function repeatPlay() {
+        switchMusic(defaultState.index);
+    }
+
+    // 随机播放
+    function shufflePlay() {
+        IcePlayer.utils.removeClass(listOne[defaultState.index], "current_playing");
+        defaultState.index = Math.floor(Math.random() * options.playList.length);
+        switchMusic(defaultState.index);
+    }
+
+    // 播放模式
+    function ctrlPlayMod() {
+        if (defaultState.playMod == 0) {
+            defaultState.playMod = 1;
+            ctrlMod.title = "单曲循环";
+            ctrlModImg.src = "../images/repeat.png";
+        } else if (defaultState.playMod == 1) {
+            defaultState.playMod = 2;
+            ctrlMod.title = "随机播放";
+            ctrlModImg.src = "../images/shuffle.png";
+        } else {
+            defaultState.playMod = 0;
+            ctrlMod.title = "顺序播放";
+            ctrlModImg.src = "../images/order.png";
+        }
+    }
 }
